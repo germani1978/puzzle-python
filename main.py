a9c5462501d85a433904bb9b8cf34d869727c970
germani <germanibenitezalfonso1@gmail.com>
2023-02-06 12:50:14 -0500

    segundo

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-def init():
-    print('Hello World')
+import pandas
+
+def cargar_palabras():
+    date = pandas.read_csv('all_word.csv')
+    #colocarlas en una array
+    
+def llena_puzzle():
+    hash
+    #crea un ciclo hasta que el puzzle se llene o no se pueda poner mas palabras    
+        #escoge una palabra aleatoria
+        #mira a ver si la puedes poner en un lugar(random)
+        #agrega el significado a las preguntas
     
 if __name__ == "__main__":
-    init()
+    cargar_palabras()
+    
