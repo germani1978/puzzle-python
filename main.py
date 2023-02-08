670b7ca3ae26e3569e24c404c1678121f6844ab0
germani <germanibenitezalfonso1@gmail.com>
2023-02-08 14:03:13 -0500

    palabras aleatorias en el puzzle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import random
 
 arr = []
 matriz = [] #primer elemnto son las x, despues las yWALL
@@ -11,21 +12,18 @@ EMPTY = ""
 nWORDS = 5000
 used_words = []
 
-#carga 5000 palabras en arr. [english, spanish]
-def cargar_palabras():
+def cargar_palabras(): #carga 5000 palabras en arr. [english, spanish]
     date = pd.read_csv('all_word.csv')
     global arr
     arr = date.values
     
-#inicia la matriz de SIZExSIZE en ""
-def iniciar_matriz():
+def iniciar_matriz(): #inicia la matriz de SIZExSIZE en ""
     global matriz
     global used_words
     matriz = [[EMPTY for x in range(SIZE)] for y in range(SIZE)]
     used_words = [ False for i in range(nWORDS)]
     
-#cantidad de vacios
-def n_empty():
+def n_empty(): #cantidad de vacios
     cant = 0
     for x in range(SIZE):
         for y in range(SIZE):
@@ -33,16 +31,14 @@ def n_empty():
                 cant+=1
     return cant
 
-#quita los vacios
-def remove_empty():
+def remove_empty(): #quita los vacios
     global matriz
     for x in range(SIZE):
         for y in range(SIZE):
            if matriz[x][y] == EMPTY:
                matriz[x][y] = WALL
 
-#averigua si la palabra cabe. Y tiene un espacio despues de ella
-def word_fit(id_word,x,y,horizontal):
+def word_fit(id_word,x,y,horizontal): #averigua si la palabra cabe. Y tiene un espacio despues de ella
     
     dx = 0
     dy = 0
@@ -62,8 +58,7 @@ def word_fit(id_word,x,y,horizontal):
     
     return True
 
-#coloca la palabra y una marca despues de ella para marcar el fin de la palabra
-def put_word(id_word,x,y,horizontal):
+def put_word(id_word,x,y,horizontal): #coloca la palabra y una marca despues de ella para marcar el fin de la palabra
     
     global matriz
     global words
@@ -93,8 +88,7 @@ def put_word(id_word,x,y,horizontal):
     else:
         words_vertical.append(id_word)
 
-#buscando una posicion para la palabra x
-def find_pos_word(id_word):
+def find_pos_word(id_word): #buscando una posicion para la palabra x
     for y in range(SIZE):
         for x in range(SIZE):
             if word_fit(id_word,x,y,True):
@@ -105,19 +99,12 @@ def find_pos_word(id_word):
                     return True
     return False
      
-#buscando palabras que quepan en el cruzigrama hasta que no haya mas espacio o queden n espacios                
-def llena_puzzle():
+def llena_puzzle(): #buscando palabras que quepan en el cruzigrama hasta que no haya mas espacio o queden n espacios
     global arr
-    for id_word in range(nWORDS):
-    # for id_word in len(arr):
-        if find_pos_word(id_word):
-            empty_spaces = n_empty()
-            if empty_spaces < 3:
-                remove_empty()
-                print(n_empty())
-                return
-                    
-  
+    while n_empty() > 2 :
+        id_word = random.randint(0,nWORDS-1)
+        find_pos_word(id_word)
+    remove_empty()
     
 if __name__ == "__main__":
     cargar_palabras()
